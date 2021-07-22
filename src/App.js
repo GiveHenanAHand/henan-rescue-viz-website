8cc73cc3b3857753392469c02cb3949a8031e20f
Ray_home <mingrui@uw.edu>
2021-07-22 11:06:48 -0700

    add Slider

--- src/App.js
+++ src/App.js
@@ -5,37 +5,60 @@ import {useState} from "react";
 
 function App() {
     const [timePreference, setTimePreference] = useState("see_all")
+    const [timeRange, setTimeRange] = useState(8)
 
-    const currentTimestamp = Date.now()
-    let latest_data = []
-    let i;
-    for (i = 0 ; i < data.length ; ++i) {
-        if (currentTimestamp - Date.parse(data[i]["Time"]) < 6 * 60 * 60 * 1000) {
-            latest_data.push(data[i])
+    let filterData = () => {
+        var filtered_data = []
+        if (timeRange == 12) {
+            filtered_data = data
+        } else {
+            const currentTimestamp = Date.now()
+            for (let i = 0 ; i < data.length ; ++i) {
+                if (currentTimestamp - Date.parse(data[i]["Time"]) < timeRange * 60 * 60 * 1000) {
+                    filtered_data.push(data[i])
+                }
+            }
         }
+        return filtered_data.map(drawPoints)
     }
-    let drawPoints = (record) => <Marker position={{lng: record["location"]["lng"] + Math.random()/1000, lat: record["location"]["lat"] + Math.random()/1000}}>
+
+    let drawPoints = (record) => <Marker position={
+        {lng: record["location"]["lng"] + Math.random()/1000, lat: record["location"]["lat"] + Math.random()/1000}
+    }>
         <InfoWindow content={
-            `<div>${record["post"]}</div><div>原微博：<a target="_blank" rel="noopener noreferrer" href=${record["link"]}>${record["link"]}</a></div><div>发布时间: 7月${record["Time"].substring(8,10)}日 ${record["Time"].substring(11, 20)}</div>`} offset={{width: 0, height: -20}}/>
+            `
+            <div>${record["post"]}</div>
+            <div>原微博：<a target="_blank" rel="noopener noreferrer" href=${record["link"]}>${record["link"]}</a></div>
+            <div>发布时间: 7月${record["Time"].substring(8,10)}日 ${record["Time"].substring(11, 20)}</div>
+            `
+        } offset={{width: 0, height: -20}}/>
     </Marker>
-    let timePreferenceOnChange = (selection) => {console.log(selection.target.value); setTimePreference(selection.target.value)}
+
+    let slider = () => {
+        var labelText = "最近"+timeRange+"小时"
+        if (timeRange == 12) {
+            labelText = "全部记录"
+        }
+        return <label>
+                    <input id="sliderRange" type="range" min="2" max="12" value={timeRange} onChange={handleSliderChange} step="2"/>
+                    {labelText}
+               </label>
+    }
+
+    let handleSliderChange = (e) => {
+        setTimeRange(e.target.value)
+    }
+
     return (
         <div className={"rootDiv"}>
             <div className="info">
                 <div>本网站仅聚合新浪微博上发布的有关2021年7月河南暴雨的求助信息，请大家注意辨别信息真伪。点击标记点可以看到更多信息及原微博地址。</div>
-                <br/>
-                <input type="radio" id="see_all" name="see_all" value="see_all"
-                       checked={timePreference === "see_all"}
-                       onChange={timePreferenceOnChange}/>
-                <label htmlFor="see_all">全部记录</label>
-                <input type="radio" id="see_latest" name="see_latest" value="see_latest"
-                       checked={timePreference === "see_latest"}
-                       onChange={timePreferenceOnChange}/>
-                <label htmlFor="see_latest">只看最近六小时</label>
+                <br/>    
+                {slider()}
             </div>
+
             <BaiduMap defaultZoom={9} defaultCenter={{lng:113.802193, lat:34.820333}} mapContainer={<div className={"mapDiv"}/>}>
-                {(timePreference === "see_all") && data.map(drawPoints)}
-                {(timePreference === "see_latest") && latest_data.map(drawPoints)}
+                {filterData()}
                 <NavigationControl
                     type="small"
                     anchor="top_right"
