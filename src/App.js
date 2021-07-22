49fe1dd56bda338dd816bbd7dc1395eba9c3a8a6
Tianshi Li <9413fi@gmail.com>
2021-07-22 00:23:44 -0700

    add disclaimer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import data from './parse_json.json'
 function App() {
     return (
         <div className={"rootDiv"}>
+            <div className="info">本网站仅聚合新浪微博上发布的有关2021年7月河南暴雨的求助信息，请大家注意辨别信息真伪。点击标记点可以看到更多信息及原微博地址。</div>
             <BaiduMap defaultZoom={9} defaultCenter={data[0]["location"]} mapContainer={<div className={"mapDiv"}/>}>
                 {data.map((record) => <Marker position={{lng: record["location"]["lng"] + Math.random()/10, lat: record["location"]["lat"] + Math.random()/10}}>
                     <InfoWindow content={
