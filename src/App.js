0adc5293223256291f14982675ad2992f34a6f88
Tianshi Li <9413fi@gmail.com>
2021-07-22 00:09:10 -0700

    initial version (point + infowindow)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {BaiduMap, Marker, InfoWindow, NavigationControl} from 'react-baidu-maps';
+import data from './parse_json.json'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={"rootDiv"}>
+            <BaiduMap defaultZoom={9} defaultCenter={data[0]["location"]} mapContainer={<div className={"mapDiv"}/>}>
+                {data.map((record) => <Marker position={{lng: record["location"]["lng"] + Math.random()/10, lat: record["location"]["lat"] + Math.random()/10}}>
+                    <InfoWindow content={
+                        `<div>${record["post"]}</div><div>原微博：<a href=${record["link"]}>${record["link"]}</a></div>`} offset={{width: 0, height: -20}}/>
+                </Marker>)}
+                <NavigationControl
+                    type="small"
+                    anchor="top_right"
+                    offset={{ width: 0, height: 30 }} />
+            </BaiduMap>
+        </div>
+    );
 }
 
 export default App;
