e1ff85a71140f6c76fc6833caa87b612f1d3671d
Tianshi Li <9413fi@gmail.com>
2021-07-22 01:29:29 -0700

    add post time

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
             <BaiduMap defaultZoom={9} defaultCenter={data[0]["location"]} mapContainer={<div className={"mapDiv"}/>}>
                 {data.map((record) => <Marker position={{lng: record["location"]["lng"] + Math.random()/10, lat: record["location"]["lat"] + Math.random()/10}}>
                     <InfoWindow content={
-                        `<div>${record["post"]}</div><div>原微博：<a target="_blank" rel="noopener noreferrer" href=${record["link"]}>${record["link"]}</a></div>`} offset={{width: 0, height: -20}}/>
+                        `<div>${record["post"]}</div><div>原微博：<a target="_blank" rel="noopener noreferrer" href=${record["link"]}>${record["link"]}</a></div><div>发布时间: 7月${record["Time"].substring(8,10)}日 ${record["Time"].substring(11, 20)}</div>`} offset={{width: 0, height: -20}}/>
                 </Marker>)}
                 <NavigationControl
                     type="small"
