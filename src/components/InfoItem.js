772245361ffd78de7304f82f1e97ad68d55fef3c
Halcao <nyz1500@gmail.com>
2021-07-25 11:30:43 +0800

    update style & add category tag

--- src/components/InfoItem.js
+++ src/components/InfoItem.js
@@ -1,10 +1,37 @@
+import { Tag, Row, Col } from "antd"
+
 function InfoItem(props) {
+    props.info.category = props.info.category || '其他'
+
+    const arr = props.info.category.split('_')
+    const category = arr[0]
+    let type = ''
+    if (arr.length > 1) {
+        type = arr[1]
+    }
+
+    const colorMap = {
+        '求助': 'red',
+        '求救': '#EC2215',
+        '提供帮助': '#47AE53',
+        '帮助': '#47AE53',
+        '其他': '#3371BE',
+    }
+
+    const color = colorMap[category]
+
     return <div className="info-item">
-        <div className="info-item-content">${props.info.post}</div>
-        <div className="info-item-footer">
-            <a className="info-item-link" href={props.info.link} target="_blank">原微博</a>
-            <div className="info-item-date">{ `发布时间: 7月${props.info.Time.substring(8, 10)}日 ${props.info.Time.substring(11, 20)}` }</div>
-        </div>
+        <div className="info-item-content">{props.info.post}</div>
+        <Row className="info-item-footer">
+            <Col className="info-item-date">{ `7月${props.info.Time.substring(8, 10)}日 ${props.info.Time.substring(11, 20)}` }</Col>
+            <Col >
+                <a className="info-item-link" href={props.info.link} target="_blank" rel="noopener noreferrer">原微博</a>
+            </Col>
+            <Col className="info-item-tag-list">
+              <Tag color={color}>{category}</Tag>
+                { type.length > 0 ? <Tag color={color}>{type}</Tag> : null }
+            </Col>
+        </Row>
     </div>
 }
 
