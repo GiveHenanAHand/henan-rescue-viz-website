cf53d4294f883f7441a67068aeb22a4fc76699fc
GeoLibra <674530915@qq.com>
2021-07-26 22:48:17 +0800

    fix: update InfoWindow

--- src/components/InfoWindow.js
+++ src/components/InfoWindow.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { Card, Button } from "antd";
-import {CloseCircleOutlined} from '@ant-design/icons';
-import {CustomOverlay} from 'react-bmapgl';
+import { CloseCircleOutlined } from '@ant-design/icons';
+import { CustomOverlay } from 'react-bmapgl';
 import InfoItem from "./InfoItem";
 import '../styles/InfoWindow.css'
 
 function InfoWindow(props) {
-
+    console.log('props', props);
     if (typeof (props.item) === 'undefined') return null
 
     return <CustomOverlay
-        position={props.item.location}
+        position={props.item.geometry.coordinates}
         autoViewport={props.shouldAutoCenter}>
-            <Card style={ { width: 320 } }>
+        <Card style={{ width: 320 }}>
             <Button type="text" danger className="windowCloseBtn" onClick={props.onCloseClick}>
                 <CloseCircleOutlined />
             </Button>
-            <InfoItem info={props.item} key={props.item.id}/>
-            </Card>
-        </CustomOverlay>
+            <InfoItem info={props.item.properties} key={props.item.properties.id} />
+        </Card>
+    </CustomOverlay>
 }
 
 export default InfoWindow
